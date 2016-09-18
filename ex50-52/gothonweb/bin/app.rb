f5ce9d7b73d47d07ee58c84095cac3d28b942fa4
lujanfernaud <lujanfernaud@gmail.com>
2016-09-18 16:39:41 +0100

    Add some game logic and views

--- ex50-52/gothonweb/bin/app.rb
+++ ex50-52/gothonweb/bin/app.rb
@@ -1,45 +1,44 @@
 require 'sinatra'
-
-include FileUtils::Verbose
+require './lib/gothonweb/map.rb'
 
 configure do
   set :port, 8080
   set :static, true
   set :public_folder, "static"
   set :views, "views"
-  enable :sessions
-end
 
-helpers do
-  def img(picture)
-    unless picture.is_a?(String)
-      "<img src='/images/#{picture[:filename]}' alt='#{picture[:filename]}' />"
-    else
-      "I can't greet you properly because you didn't upload your picture."
-    end
-  end
+  enable :sessions
+  set    :session_secret, 'BADSECRET'
 end
 
 get '/' do
-  erb :index
+  session[:room] = 'START'
+  redirect '/game'
 end
 
-get '/hello/' do
-  erb :hello_form
-end
+get '/game' do
+  room = Map::load_room(session)
 
-post '/hello/' do
-  greeting = params[:greeting] || "Hi there"
-  name = params[:name] || "John Doe"
-  picture = params[:picture] || "No picture uploaded."
-
-  unless picture.is_a?(String)
-    tempfile = params[:picture][:tempfile]
-    filename = params[:picture][:filename]
-    cp(tempfile.path, "static/images/#{filename}")
+  if room
+    erb :show_room, :locals => {:room => room}
+  else
+    erb :you_died
   end
+end
 
-  erb :greeting, :locals => {'greeting' => greeting, 
-                          'name' => name,
-                          'picture' => picture}
+post '/game' do
+  room   = Map::load_room(session)
+  action = params[:action]
+
+  if room
+    next_room = room.go(action) || room.go('*')
+
+    if next_room
+      Map::save_room(session, next_room)
+    end
+
+    redirect '/game'
+  else
+    erb :you_died
+  end
 end
