c62cb8629211c7d16d62b396a3e6d314ca28b25d
lujanfernaud <lujanfernaud@gmail.com>
2016-09-20 08:17:45 +0100

    Add 'does not compute'

--- ex50-52/gothonweb/bin/app.rb
+++ ex50-52/gothonweb/bin/app.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
 require './lib/gothonweb/map.rb'
 
-require 'pry'
+require 'pry-byebug'
+require 'pry-inline'
 
 configure do
   set :port, 8080
@@ -11,6 +12,24 @@ configure do
 
   enable :sessions
   set    :session_secret, 'BADSECRET'
+
+  @@error = false
+end
+
+helpers do
+  def does_not_compute?
+    @@error
+  end
+
+  def last_death_line
+    quips = [
+      "You died. You kinda suck at this.",
+      "You died. Such a luser.",
+      "I have a small puppy that's better at this."
+    ]
+
+    quips[rand(0..(quips.length-1))]
+  end
 end
 
 get '/' do
@@ -30,11 +49,17 @@ get '/game' do
 end
 
 post '/game' do
+  @@error = false
+
   room   = Map::load_room(session)
   action = params[:action].downcase
 
   if room
-    next_room = room.go(action) || room.go('*')
+    if room.go(action) != "not compute"
+      next_room = room.go(action) || room.go('*')
+    else
+      @@error = true
+    end
 
     if next_room
       Map::save_room(session, next_room)
