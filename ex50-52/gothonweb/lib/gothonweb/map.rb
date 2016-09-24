bc8a1a5f6ef123ce0f1f96a5ec83af9494223366
lujanfernaud <lujanfernaud@gmail.com>
2016-09-24 11:31:52 +0100

    Update descriptions

--- ex50-52/gothonweb/lib/gothonweb/map.rb
+++ ex50-52/gothonweb/lib/gothonweb/map.rb
@@ -48,35 +48,49 @@ module Map
 
   CENTRAL_CORRIDOR = Room.new("Central Corridor",
     """
+    <p>
     The Gothons of Planet Percal #25 have invaded your ship and destroyed
-    your entire crew.  You are the last surviving member and your last
+    your entire crew. You are the last surviving member and your last
     mission is to get the neutron destruct bomb from the Weapons Armory,
     put it in the bridge, and blow the ship up after getting into an 
     escape pod.
-
+    </p>
+    <p>
     You're running down the central corridor to the Weapons Armory when
     a Gothon jumps out, red scaly skin, dark grimy teeth, and evil clown costume
-    flowing around his hate filled body.  He's blocking the door to the
+    flowing around his hate filled body. He's blocking the door to the
     Armory and about to pull a weapon to blast you.
+    </p>
     """,
     true) # actions
 
   LASER_WEAPON_ARMORY = Room.new("Laser Weapon Armory",
     """
+    <p>
     Lucky for you they made you learn Gothon insults in the academy.
     You tell the one Gothon joke you know:
+    </p>
+    <p>
     Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr, fur fvgf nebhaq gur ubhfr.
+    </p>
+    <p>
     The Gothon stops, tries not to laugh, then busts out laughing and can't move.
     While he's laughing you run up and shoot him square in the head
     putting him down, then jump through the Weapon Armory door.
-
+    </p>
+    <p>
     You do a dive roll into the Weapon Armory, crouch and scan the room
-    for more Gothons that might be hiding.  It's dead quiet, too quiet.
+    for more Gothons that might be hiding. It's dead quiet, too quiet.
     You stand up and run to the far side of the room and find the
-    neutron bomb in its container.  There's a keypad lock on the box
-    and you need the code to get the bomb out.  If you get the code
-    wrong 10 times then the lock closes forever and you can't
-    get the bomb.  The code is 3 digits.
+    neutron bomb in its container.
+    </p>
+    <p>
+    There's a keypad lock on the box and you need the code to get the bomb out. If you get the code
+    wrong 6 times then the lock closes forever and you can't get the bomb.
+    </p>
+    <p>
+    The code is 3 digits.
+    </p>
     """,
     false, # actions
     true)  # keypad
@@ -85,21 +99,30 @@ module Map
 
   THE_BRIDGE = Room.new("The Bridge", 
     """
+    <p>
+    CLICK!
+    </p>
+    <p>
     The container clicks open and the seal breaks, letting gas out.
+    </p>
+    <p>
     You grab the neutron bomb and run as fast as you can to the
     bridge where you must place it in the right spot.
-
+    </p>
+    <p>
     You burst onto the Bridge with the netron destruct bomb
     under your arm and surprise 5 Gothons who are trying to
-    take control of the ship.  Each of them has an even uglier
-    clown costume than the last.  They haven't pulled their
+    take control of the ship. Each of them has an even uglier
+    clown costume than the last. They haven't pulled their
     weapons out yet, as they see the active bomb under your
     arm and don't want to set it off.
+    </p>
     """,
     true) # actions
 
   ESCAPE_POD = Room.new("Escape Pod", 
     """
+    <p>
     You point your blaster at the bomb under your arm
     and the Gothons put their hands up and start to sweat.
     You inch backward to the door, open it, and then carefully
@@ -108,14 +131,20 @@ module Map
     and blast the lock so the Gothons can't get out.
     Now that the bomb is placed you run to the escape pod to
     get off this tin can.
-
+    </p>
+    <p>
     You rush through the ship desperately trying to make it to
-    the escape pod before the whole ship explodes.  It seems like
+    the escape pod before the whole ship explodes. It seems like
     hardly any Gothons are on the ship, so your run is clear of
-    interference.  You get to the chamber with the escape pods, and
-    now need to pick one to take.  Some of them could be damaged
-    but you don't have time to look.  There's 5 pods, which one
-    do you take?
+    interference.
+    <br />
+    You get to the chamber with the escape pods, and
+    now need to pick one to take. Some of them could be damaged
+    but you don't have time to look.
+    </p>
+    <p>
+    There are 5 pods, which one do you take?
+    </p> 
     """,
     false, # actions
     true)  # keypad
@@ -124,68 +153,87 @@ module Map
 
   THE_END_WINNER = Room.new("The End",
     """
+    <p>
     You jump into pod #{ESCAPE_POD.good_door} and hit the eject button.
     The pod easily slides out into space heading to
-    the planet below.  As it flies to the planet, you look
+    the planet below.
+    </p>
+    <p>
+    As it flies to the planet, you look
     back and see your ship implode then explode like a
     bright star, taking out the Gothon ship at the same
-    time.  You won!
+    time.
+    </p>
+    <h2>You won!</h2>
     """)
 
   THE_END_LOSER_1 = Room.new("Dead",
     """
+    <p>
     You jump into a random pod and hit the eject button.
     The pod escapes out into the void of space, then
     implodes as the hull ruptures, crushing your body
     into jam jelly.
+    </p>
     """)
 
   THE_END_LOSER_2 = Room.new("Dead",
     """
+    <p>
     The ship explodes.
+    </p>
     """)
 
   SHOOT_DEATH = Room.new("Dead", 
     """
+    <p>
     Quick on the draw you yank out your blaster and fire it at the Gothon.
     His clown costume is flowing and moving around his body, which throws
     off your aim. Your laser hits his costume but misses him entirely. This
     completely ruins his brand new costume his mother bought him, which
     makes him fly into an insane rage and blast you repeatedly in the face until
     you are dead. Then he eats you.
-    \n
+    </p>
     """)
   
   DODGE_DEATH = Room.new("Dead", 
     """
+    <p>
     Like a world class boxer you dodge, weave, slip and slide right
     as the Gothon's blaster cranks a laser past your head.
     In the middle of your artful dodge your foot slips and you
     bang your head on the metal wall and pass out.
+    </p>
+    <p>
     You wake up shortly after only to die as the Gothon stomps on
     your head and eats you.
-    \n
+    </p>
     """)
 
   WRONG_CODE_DEATH = Room.new("Dead", 
     """
+    <p>
     The lock buzzes one last time and then you hear a sickening
     melting sound as the mechanism is fused together.
+    </p>
+    <p>
     You decide to sit there, and finally the Gothons blow up the
     ship from their ship and you die.
-    \n
+    </p>
     """)
 
   BOMB_DEATH = Room.new("Dead", 
     """
+    <p>
     In a panic you throw the bomb at the group of Gothons
     and make a leap for the door. Right as you drop it a
     Gothon shoots you right in the back killing you.
-    \n
+    </p>
+    <p>
     As you die you see how another Gothon frantically try to disarm
     the bomb. You die knowing they will probably blow up when
     it goes off.
-    \n
+    </p>
     """)
 
   # Now we connect the rooms using Room.add_paths(paths).
