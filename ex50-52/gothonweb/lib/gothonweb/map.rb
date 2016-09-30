95213b5e83657ea7df478d1f86c91010904ffb9c
lujanfernaud <lujanfernaud@gmail.com>
2016-09-30 08:24:00 +0100

    Small fix

--- ex50-52/gothonweb/lib/gothonweb/map.rb
+++ ex50-52/gothonweb/lib/gothonweb/map.rb
@@ -160,7 +160,8 @@ module Map
     the escape pod before the whole ship explodes. It seems like
     hardly any Gothons are on the ship, so your run is clear of
     interference.
-    <br />
+    <p>
+    </p>
     You get to the chamber with the escape pods, and
     now need to pick one to take. Some of them could be damaged
     but you don't have time to look.
