0ea218f14cf66d9120a9f86343b085c101ddc717
lujanfernaud <lujanfernaud@gmail.com>
2016-11-05 07:17:45 +0000

    Clean and refactor

--- ex50-52/gothonweb/bin/application_helpers.rb
+++ ex50-52/gothonweb/bin/application_helpers.rb
@@ -67,17 +67,17 @@ module ApplicationHelpers
 
   def check_guesses
 
-    def find_error_message(first_messages=false, last_messages=false)
+    def find_error_message(first_messages = false, last_messages = false)
       @@error_messages_list = [
-        "If you do what you always did, you will get what you always got.",                        #[0]
-        "That's not the code.",                                                                    #[1]
-        "Success is walking from failure to failure with no loss of enthusiasm.",                  #[2]
-        "Wrong!",                                                                                  #[3]
-        # Last messages used when the last_message modifier is true:
-        "Time is a drug. Too much of it kills you.",                                               #[-4]
-        "Million-to-one chances...crop up nine times out of ten.",                                 #[-3]
-        "In the space between chaos and shape there was another chance.",                          #[-2]
-        "You're running out of opportunities."                                                     #[-1]
+        "If you do what you always did, you will get what you always got.",       # [0]
+        "That's not the code.",                                                   # [1]
+        "Success is walking from failure to failure with no loss of enthusiasm.", # [2]
+        "Wrong!",                                                                 # [3]
+        # Last messages used when the last_message modifier is true :
+        "Time is a drug. Too much of it kills you.",                              # [-4]
+        "Million-to-one chances...crop up nine times out of ten.",                # [-3]
+        "In the space between chaos and shape there was another chance.",         # [-2]
+        "You're running out of opportunities."                                    # [-1]
       ]
 
       if first_messages
@@ -100,13 +100,13 @@ module ApplicationHelpers
     end
 
     if @@guesses == 1
-      "<p>#{find_error_message(first_messages=true)}</p>
+      "<p>#{find_error_message(first_messages = true)}</p>
       <p>Tries left: #{6 - @@guesses}</p>"
     elsif @@guesses > 0 && @@guesses != 6
       "<p>#{find_error_message}</p>
       <p>Tries left: #{6 - @@guesses}</p>"
     elsif @@guesses == 6
-      "<p>#{find_error_message(first_messages=false, last_messages=true)}</p>
+      "<p>#{find_error_message(first_messages = false, last_messages = true)}</p>
       <p>Last try!</p>"
     end
   end
@@ -124,8 +124,8 @@ module ApplicationHelpers
         n
       # If we are in round 3 and we have asterisk == 0 (the previous character is a number),
       # then we put another number. Otherwise we would have two asterisks and it would be
-      # too difficult to guess the number.  
-      elsif asterisk == 0 && round == 3
+      # too difficult to guess the number.
+      elsif asterisk.zero? && round == 3
         n
       else
         asterisk = 1
@@ -135,20 +135,20 @@ module ApplicationHelpers
     hint.join.to_s
   end
 
-  def show_door_hint(good_door, bad_door=nil, total_doors)
+  def show_door_hint(good_door, bad_door = nil, total_doors)
     good_door_number = [good_door.to_i]
     bad_door_number  = [bad_door.to_i] if bad_door
     doors_array      = [*1..total_doors]
-    
-    if bad_door
-      hint = doors_array - good_door_number - bad_door_number
-    else
-      hint = doors_array - good_door_number
-    end
-    
+
+    hint = if bad_door
+             doors_array - good_door_number - bad_door_number
+           else
+             doors_array - good_door_number
+           end
+
     switch = rand(0..1)
 
-    if switch == 0
+    if switch.zero?
       hint.shift
     else
       hint.pop
@@ -158,7 +158,7 @@ module ApplicationHelpers
   end
 
   def reset_buzz_guesses_hint_and_door
-    @@activate_buzz = false      
+    @@activate_buzz = false
     @@guesses       = 0
     @@hint_counter  = 0
     @@door_locked   = false
